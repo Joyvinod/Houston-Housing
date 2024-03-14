9dca814126911bcd69395ddec00f6ceff2d83f22
Swain, Evan Kaoru <eswain7@gatech.edu>
2024-03-14 18:32:05 -0500

    Further cleaning of data
    Starting from Joy's second step of cleaned data, this code removes columns with more than 5% of data missing and removes rows with missing values in the resulting columns. It also converts factor columns to factors, replacing two-level factors with 0 or 1. Half baths are separated out as an independent column from BathsTotal.

---
+++ Code/20230314_data_cleaning.R
@@ -0,0 +1,68 @@
+
+### load Joy's data (second step of cleaning)
+data2 <- read.csv("/Users/eswain7/Documents/Documents - ES’s MacBook Air/202401_MGT6203/Project/data_cleaned_stage2.csv")
+
+# install libraries
+install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+library(tidyverse)
+
+### subset df to columns with less than 5% missing and remove rows with NAs
+
+# set maximum number of NAs in column as 5% of observations
+max_na <- nrow(data2)*0.05
+
+# select columns with less NAs than max_na
+data4 <- data2[, colSums(is.na(data2)) < max_na]
+
+# identify columns with NAs
+colnames(data4[, colSums(is.na(data4)) > 0])
+
+# identify rows with more than one NA and remove
+missing_many <- as.integer((rownames(data4[rowSums(is.na(data4)) > 0, ])))
+data4 <- data4[-missing_many, ]      # removes 363 observations
+
+
+### clean by column
+
+# split baths into full and half columns
+data4 <- data4 %>%
+  mutate(HalfBathsTotal = (BathsTotal %% 1)*10) %>%
+  mutate(BathsTotal = floor(BathsTotal)) 
+
+# factors to factors by column
+data4 <- data4 %>%
+  # BathsTotal
+  # BedsTotal
+  # SqFtTotal
+  # CloseDate
+  # ClosePrice
+  mutate(Siding = as.factor(Siding)) %>%
+  mutate(CoolSystem = as.factor(CoolSystem)) %>%
+  mutate(CountyOrParish = as.factor(CountyOrParish)) %>%
+  mutate(ForLease = as.factor(ifelse(ForLease == "True", 1, 0))) %>%
+  mutate(HeatSystem = as.factor(HeatSystem)) %>%
+  mutate(HOAMandatory = as.factor(ifelse(HOAMandatory == "True", 1, 0))) %>%
+  mutate(GeoMarketArea = as.factor(GeoMarketArea)) %>%
+  mutate(NewConstruction = as.factor(ifelse(NewConstruction == "True", 1, 0))) %>%
+  mutate(PoolPrivate = as.factor(ifelse(PoolPrivate == "True", 1, 0))) %>%
+  mutate(PropertyType = as.factor(PropertyType)) %>%
+  mutate(DwellingType = as.factor(DwellingType)) %>%
+  mutate(Restrictions = as.factor(Restrictions)) %>%
+  mutate(Roof = as.factor(Roof)) %>%
+  mutate(SchoolDistrict = as.factor(SchoolDistrict)) %>%
+  mutate(WaterSewer = as.factor(WaterSewer)) %>%
+  # YearBuilt
+  mutate(PostalCode = as.factor(PostalCode))
+  # HalfBathsTotal
+
+# remove lease properties
+data4 <- data4[data4$ForLease == 0, ]
+
+# remove ForLease and PropertyType since uniform values
+data4 <- data4 %>% select(-c(ForLease, PropertyType))
+
+# order by close date
+data4 <- data4[order(data4$CloseDate),]
+
+# export to csv
+write.csv(data4, "/Users/eswain7/Documents/Documents - ES’s MacBook Air/202401_MGT6203/Project/Upload/20240314_cleaned_data")
